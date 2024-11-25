8e3dae90800b23e5063c42c98710c2335baea93e
sameboy0 <ryanundertale@gmail.com>
2024-11-25 20:03:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    const menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+  });
